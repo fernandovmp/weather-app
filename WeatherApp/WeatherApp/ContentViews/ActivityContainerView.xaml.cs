77a0a1f5f72ed16f5efb060e6ff5b787e2f4f504
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-01-13 20:53:18 -0300

    Add activity indicator to WeatherPage and Fallback value to humidity, wind and clouds data

---
+++ WeatherApp/WeatherApp/ContentViews/ActivityContainerView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.ContentViews
+{
+    [ContentProperty(nameof(ContentElement))]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActivityContainerView : ContentView
+    {
+        public ActivityContainerView()
+        {
+            InitializeComponent();
+        }
+
+        public static BindableProperty IsLoadingProperty = BindableProperty.Create(nameof(IsLoading),
+            typeof(bool), typeof(ActivityContainerView), propertyChanged: IsLoadingChanged);
+        public static BindableProperty ContentElementProperty = BindableProperty.Create(nameof(ContentElement),
+            typeof(View), typeof(ActivityContainerView), propertyChanged: OnContentElementChanged);
+
+        public bool IsLoading
+        {
+            get => (bool) GetValue(IsLoadingProperty);
+            set => SetValue(IsLoadingProperty, value);
+        }
+        public View ContentElement
+        {
+            get => (View)GetValue(ContentElementProperty);
+            set => SetValue(ContentElementProperty, value);
+        }
+        private static void IsLoadingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var activityContainer = bindable as ActivityContainerView;
+            bool isLoading = (bool) newValue;
+            ActivityIndicator indicator = activityContainer.activityIndicator;
+            indicator.IsVisible = isLoading;
+            indicator.IsRunning = isLoading;
+            activityContainer.contentView.IsVisible = !isLoading;
+        }
+        private static void OnContentElementChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var activityContainer = bindable as ActivityContainerView;
+            View content = newValue as View;
+            activityContainer.contentView.Content = content;
+        }
+    }
+}
