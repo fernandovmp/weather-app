c9b7dac15bc50c6b9aa1acf93b64b4cd000ae6e5
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-01-13 16:23:32 -0300

    Add app icon and release configuration

--- WeatherApp/WeatherApp.Android/MainActivity.cs
+++ WeatherApp/WeatherApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Lottie.Forms.Droid;
 
 namespace WeatherApp.Droid
 {
-    [Activity(Label = "WeatherApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Weather", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
